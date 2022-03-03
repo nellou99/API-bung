e2764cbf82582db399da4f7d9d509d93b08a6662
nellou99 <nellyndankou@gmail.com>
2022-03-03 18:34:55 +0100

    initialisation

---
+++ src/main/java/com/example/api_test_nellynd/ApiTestNellyNdApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_test_nellynd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestNellyNdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestNellyNdApplication.class, args);
+    }
+
+}
